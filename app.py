c43927e463e1e4ea24b1a2bb1143740db80e06fb
王昱 <wangyu22@xiaomi.com>
2024-10-27 15:53:34 +0200

    feat: Add neighbors coordinates

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import random
 import time
+from get_coordinates import get_coordinates
 
 app = Flask(__name__)
 CORS(app)
@@ -12,11 +13,12 @@ def predict_price():
     
     random_price = round(random.uniform(100, 500), 2)
 
-    time.sleep(1.5)
-
+    neighbourhood = data.get("Host_neighbourhood", "Downtown")
+    
     response_data = {
         "received_params": data,
-        "price": random_price
+        "price": random_price,
+        "neighbours": get_coordinates(neighbourhood)
     }
     
     return jsonify(response_data)
